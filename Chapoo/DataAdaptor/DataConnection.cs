d437cea07d69a4d790c1bbfa2e8aa8f07a65281e
Syv <matthijs@hartskeerl.nl>
2018-12-16 16:48:39 +0100

    Created Datalink to database.

---
+++ Chapoo/DataAdaptor/DataConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdaptor
+{
+    public class DataConnection
+    {
+
+        protected string connectionString = "Data Source=den1.mssql7.gear.host;Initial Catalog=chapoo1819f01;User ID=chapoo1819f01;Password=Pu0Q_utpxc-k";
+        protected SqlConnection connection;
+
+        public DataConnection()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        public SqlDataReader Query(string query)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand sql = new SqlCommand(query, connection);
+                return sql.ExecuteReader();
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+        }
+
+
+    }
+}
---
+++ Chapoo/DataAdaptor/KeukenBarDataConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdaptor
+{
+    public static class KeukenBarDataConnection
+    {
+        private static DataConnection db = new DataConnection();
+        
+        public static List<string> GetOpenOrders(bool isBar)
+        {
+            throw new NotImplementedException("KeukenBarDataConnection.GetOpenOrders(bool isBar) is not yet implemented");
+        }
+    }
+}
