c039bff5461010af18ae6065dcd67f2e8d76ccd9
Unknown <robbin.kragtwijk@gmail.com>
2018-12-18 17:47:00 +0100

    Rekening DAO

---
+++ Chapoo/DataAdaptor/RekeningDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataAdaptor
+{
+    public class RekeningDAO
+    {
+        public static void ZetTafelVrijmaken(int tafelId)
+        {
+            string query = "update TAFEL set Bezet = 0 where id = '@_tafelid";
+            query = query.Replace("@_tafelid", tafelId.ToString());
+
+            SqlDataReader update;
+            update = DataConnection.Query(query);
+        }
+    }
+}
