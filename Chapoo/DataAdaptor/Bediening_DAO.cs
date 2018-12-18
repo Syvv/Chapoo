bfaab7dcbd7748900ebe9d70322f1d818c78dc2b
Jesse <jessesmail.jve@gmail.com>
2018-12-18 15:53:03 +0100

    DAO voor de bediening toegevoegd

---
+++ Chapoo/DataAdaptor/Bediening_DAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdaptor
+{
+    class Bediening_DAO
+    {
+        public static List<string> GetFris()
+        {
+            List<string> result = new List<string>();
+            SqlDataReader data;
+
+            data = DataConnection.Query("SELECT * FROM MENU WHERE categorie = 'N'");
+
+            while (data.Read())
+            {
+                result.Add(data.GetFieldValue<int>(0) + " - " + data.GetFieldValue<string>(1));
+            }
+            return result;
+        }
+    }
+}
