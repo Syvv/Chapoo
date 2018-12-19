8a9df082739bb74a6bd4374a028146d5495fd629
Unknown <robbin.kragtwijk@gmail.com>
2018-12-19 19:38:57 +0100

    Menu Model

--- Chapoo/DataAdaptor/RekeningDAO.cs
+++ Chapoo/DataAdaptor/RekeningDAO.cs
@@ -11,11 +11,49 @@ namespace DataAdaptor
     {
         public static void ZetTafelVrijmaken(int tafelId)
         {
-            string query = "update TAFEL set Bezet = 0 where id = '@_tafelid";
+            string query = "update TAFEL set Bezet = 0 where id = '@_tafelid'";
             query = query.Replace("@_tafelid", tafelId.ToString());
 
             SqlDataReader update;
             update = DataConnection.Query(query);
         }
+
+        public static List<string>OphalenBestellingen(int bestellingId)
+        {
+            List<string> bestelling = new List<string>();
+            string query = "Select * from * where BestellingId = '@bestellingId' ";
+            query = query.Replace("@bestellingId", bestellingId.ToString());
+
+            SqlDataReader data = DataConnection.Query(query);
+
+            while (data.Read())
+            {
+                bestelling.Add(data.GetFieldValue<int>(0) + " - " + data.GetFieldValue<string>(1));
+            }
+            return bestelling;
+        }
+        //public static List<string>2OphalenBestellingen(int bestellingId)
+        //{
+        //    List<string> bestelling = new List<string>();
+
+        //    string query = "Select * from * where BestellingId = '@bestellingId' ";
+        //    query = query.Replace("@bestellingId", bestellingId.ToString());
+
+
+        //    return bestelling;
+        //}
+
+        public int BestellingIdOpvragen(int tafelNummer)
+        {
+            string query = "SELECT bestellingId FROM bestelling Where tafel_id = '@_tafelid'";
+            query = query.Replace("@_tafelid", tafelNummer.ToString());
+
+            SqlDataReader bestellingNummer = DataConnection.Query(query);
+            int bestellingId = int.Parse(bestellingNummer.ToString());
+
+            return bestellingId;
+        }
+
+        public 
     }
 }
---
+++ Chapoo/Model/GerechtenModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class GerechtenModel
+    {
+        public int menuId { get; set; }
+        public string menuItem { get; set; }
+        public double prijs { get; set; }
+        public char categorie { get; set; }
+        public int voorraad { get; set; }
+
+        public GerechtenModel(int menuId, string menuItem, double prijs, char categorie, int voorraad)
+        {
+            this.menuId = menuId;
+            this.menuItem = menuItem;
+            this.prijs = prijs;
+            this.categorie = categorie;
+            this.voorraad = voorraad;
+        }
+    }
+}
