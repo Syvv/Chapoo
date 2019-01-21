d12aa08b0085e6321134be1f4e704708da3e1bfe
Syv <matthijs@hartskeerl.nl>
2019-01-21 17:51:55 +0100

    Popup appears when clicking on a comment.

--- Chapoo/Logica/BarKeukenUIElement.cs
+++ Chapoo/Logica/BarKeukenUIElement.cs
@@ -23,20 +23,25 @@ namespace Logica
         public BarKeukenUIElement(Bestellingsitem b, Refresh del, int x, int y)
         {
             Item = b;
-            if (Item.Comment == "" || Item.Comment == null)
-            {
-                Item.Comment = " ";
-            }
             Controls.Add(new Label { Text = Item.Name, Top = y, Left = x, Width = 175 });
             Controls.Add(new Label { Text = Item.Amount.ToString() + "x", Top = y, Left = x + 180, Width = 20 });
             Controls.Add(new Label { Text = "Tafel " + Item.Tafel.ToString(), Top = y, Left = x +200, Width = 50});
-            Controls.Add(new Label { Text = Item.Comment.Substring(0,25), Top = y, Left = x + 250, Width = 100});
-            Controls.Add(new Button { Text = "1 item gereedstellen", Top = y, Left = x + 350, Width = 100 });
-            Controls.Add(new Button { Text = "alle items gereedstellen", Top = y, Left = x + 450, Width = 100 });
+            //Cut off the commenttext if it is longer than 25 characters and replace the last 3 characters by '...' to indicate this
+            if(Item.Comment.Length>25)
+            {
+                Controls.Add(new Label { Text = Item.Comment.Substring(0, 22) + "...", Top = y, Left = x + 250, Width = 125});
+            }
+            else
+            {
+                Controls.Add(new Label { Text = Item.Comment, Top = y, Left = x + 250, Width = 125 });
+            }
+            
+            Controls.Add(new Button { Text = "1 item gereedstellen", Top = y, Left = x + 375, Width = 100 });
+            Controls.Add(new Button { Text = "alle items gereedstellen", Top = y, Left = x + 475, Width = 100 });
 
             Controls[3].Click += (s, e) =>
             {
-                
+                MessageBox.Show(Item.Comment,Item.Name + " " + Item.Amount + "x",MessageBoxButtons.OK,MessageBoxIcon.Information);
             };
             Controls[4].Click += (s,e) => 
             {
