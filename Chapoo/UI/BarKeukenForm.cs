8b3b264ec957dd6c36a93f61073a7154e4165783
Syv <matthijs@hartskeerl.nl>
2019-01-23 13:40:07 +0100

    More UI

--- Chapoo/UI/BarKeukenForm.cs
+++ Chapoo/UI/BarKeukenForm.cs
@@ -58,13 +58,14 @@ namespace UI
                 }
 
                 //add the controls that belong to all BarKeukenUIElements so that you can actually see them
+                Controls.Add(new Label() { Text = "Commentaar", Top = 0, Left = 640, Font = new System.Drawing.Font("Arial", 16), Height = 30, Width = 200 });
                 foreach (BarKeukenUIElement ui in UIElements)
                 {
                     foreach(Control c in ui.Controls)
                     {
                         Controls.Add(c);
                     }
-                    Controls.Add(new Label() { Text = "Commentaar", Top = 0, Left = 540, Font = new System.Drawing.Font("Arial", 16), Height = 30, Width = 200 });
+                    
                 }
             }
             
