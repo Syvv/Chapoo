e7fcd72efe7e57df4eb6f66b07054775bfcfcb29
Syv <matthijs@hartskeerl.nl>
2019-01-23 22:41:18 +0100

    The smallest UI change ever

--- Chapoo/Logica/BarKeukenUIElement.cs
+++ Chapoo/Logica/BarKeukenUIElement.cs
@@ -24,22 +24,22 @@ namespace Logica
         {
             Item = b;
             //Controls.Add();
-            GroupBox gr = new GroupBox { Top = y, Left = x, Height = 50, Width = 1600, BackColor = System.Drawing.Color.LightBlue };
-            Controls.Add(new Label { Text = Item.Name, Top = y+15, Left = x + 140, Width = 650, Font = new System.Drawing.Font("Arial",16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
-            Controls.Add(new Label { Text = Item.Amount.ToString() + "x", Top = y+15, Left = x + 100, Width = 40, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
+            GroupBox gr = new GroupBox { Top = y, Left = x, Height = 50, Width = 1610, BackColor = System.Drawing.Color.LightBlue };
+            Controls.Add(new Label { Text = Item.Name, Top = y+15, Left = x + 145, Width = 650, Font = new System.Drawing.Font("Arial",16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
+            Controls.Add(new Label { Text = Item.Amount.ToString() + "x", Top = y+15, Left = x + 100, Width = 45, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
             Controls.Add(new Label { Text = "Tafel " + Item.Tafel.ToString() + ":", Top = y+15, Left = x, Width = 100, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
             //Cut off the comment_text if it is longer than 25 characters and replace the last 3 characters by '...' to indicate this
             if(Item.Comment.Length>25)
             {
-                Controls.Add(new Label { Text = "("+ Item.Comment.Substring(0, 22) + "...)", Top = y+15, Left = x + 790, Width = 300, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
+                Controls.Add(new Label { Text = "("+ Item.Comment.Substring(0, 22) + "...)", Top = y+15, Left = x + 795, Width = 300, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
             }
             else
             {
-                Controls.Add(new Label { Text = "("+Item.Comment+")", Top = y+15, Left = x + 790, Width = 300, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
+                Controls.Add(new Label { Text = "("+Item.Comment+")", Top = y+15, Left = x + 795, Width = 300, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
             }
-            Controls.Add(new Button { Text = "1 item gereedstellen", Top = y+7, Left = x + 1090, Width = 200, Font = new System.Drawing.Font("Arial", 12), Height = 40, TextAlign=System.Drawing.ContentAlignment.MiddleCenter });
-            Controls.Add(new Button { Text = "Hele rij gereedstellen", Top = y+7, Left = x + 1290, Width = 200, Font = new System.Drawing.Font("Arial", 12), Height = 40, TextAlign = System.Drawing.ContentAlignment.MiddleCenter });
-            Controls.Add(new Label { Text = Item.Timestamp.ToLongTimeString(), Top = y+15, Left = x + 1490, Width = 100, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
+            Controls.Add(new Button { Text = "1 item gereedstellen", Top = y+7, Left = x + 1095, Width = 200, Font = new System.Drawing.Font("Arial", 12), Height = 40, TextAlign=System.Drawing.ContentAlignment.MiddleCenter });
+            Controls.Add(new Button { Text = "Hele rij gereedstellen", Top = y+7, Left = x + 1295, Width = 200, Font = new System.Drawing.Font("Arial", 12), Height = 40, TextAlign = System.Drawing.ContentAlignment.MiddleCenter });
+            Controls.Add(new Label { Text = Item.Timestamp.ToLongTimeString(), Top = y+15, Left = x + 1495, Width = 100, Font = new System.Drawing.Font("Arial", 16), Height = 30, BackColor = System.Drawing.Color.LightBlue });
             foreach(Control c in Controls)
             {
                 gr.Controls.Add(c);
