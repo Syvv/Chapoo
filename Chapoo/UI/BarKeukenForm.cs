e1737256d5c8b286cc8af34aa5b60531c46fd605
Syv <matthijs@hartskeerl.nl>
2019-01-28 12:44:49 +0100

    UI updates

--- Chapoo/UI/BarKeukenForm.cs
+++ Chapoo/UI/BarKeukenForm.cs
@@ -57,8 +57,7 @@ namespace UI
                 int y = 30;
                 foreach (Bestellingsitem b in Bestellingen)
                 {
-                    BarKeukenUIElement uiElement = new BarKeukenUIElement(b,bestellingLogica,(item)=> { Bestellingen.Remove(item); BuildUI(); });
-                    uiElement.Top = y;
+                    BarKeukenUIElement uiElement = new BarKeukenUIElement(b, bestellingLogica, (item) => { Bestellingen.Remove(item); BuildUI(); }){ Top = y};
                     Controls.Add(uiElement);
                     y += 50;
                 }
